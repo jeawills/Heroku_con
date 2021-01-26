69ebee3ae6e84ec685a4773c30004973dafd2383
Jeawills <jeawills2@gmail.com>
2021-01-26 04:16:04 -0500

    deleting file

--- Solved/config.py
+++
@@ -1,5 +0,0 @@
-remote_db_endpoint = 'databootcamp.cttzzalysmo7.us-east-2.rds.amazonaws.com'
- remote_db_port = '3306'
- remote_db_user = 'admin'
- remote_db_pwd = 'He!!o5622'
- remote_db_name = 'gwsis'
